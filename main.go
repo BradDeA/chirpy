f101eafb1ce535ced59ff228c4f828d091442dc2
BradDeA <rcman808@gmail.com>
2025-07-17 14:06:22 -0400

    created /api/users endpoint

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
 
+	"github.com/BradDeA/chirpy.git/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	Db             *database.Queries
+}
+
+type RequestParams struct {
+	Body string `json:"body"`
+}
+
+type UserValues struct {
+	Id        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -36,19 +56,35 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	dbPlatform := os.Getenv("PLATFORM")
+	if dbPlatform != "dev" {
+		w.WriteHeader(403)
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
+	cfg.Db.DeleteUsers(context.Background())
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
 }
 
 func main() {
 
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
 	ServMux := http.NewServeMux()
 	server := http.Server{Addr: ":8080", Handler: ServMux}
-	apiCfg := apiConfig{}
 
-	ServMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	ServMux.Handle("/assets", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("."))))
+	db, dberr := sql.Open("postgres", dbURL)
+	if dberr != nil {
+		log.Println(dberr)
+	}
+
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{Db: dbQueries}
+
+	ServMux.Handle("GET /app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	ServMux.Handle("GET /assets", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("."))))
 	ServMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	ServMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
@@ -61,10 +97,6 @@ func main() {
 
 	ServMux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
 		profane := []string{"kerfuffle", "sharbert", "fornax"}
-
-		type RequestParams struct {
-			Body string `json:"body"`
-		}
 
 		type ReturnValues struct {
 			Error        string `json:"error"`
@@ -112,6 +144,44 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(successData)
+	})
+
+	ServMux.HandleFunc("POST /api/users", func(w http.ResponseWriter, r *http.Request) {
+
+		type EmailStruct struct {
+			Email string `json:"email"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := EmailStruct{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		user, userErr := apiCfg.Db.CreateUser(context.Background(), params.Email)
+		if userErr != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		marshalValues := UserValues{Id: user.ID, CreatedAt: user.CreatedAt, UpdatedAt: user.UpdatedAt, Email: user.Email}
+		fmt.Println(json.Marshal(marshalValues))
+		fmt.Printf("%T\n", marshalValues)
+		jsonData, err := json.Marshal(marshalValues)
+		fmt.Println(string(jsonData), err)
+
+		returnData, marshalErr := json.Marshal(marshalValues)
+		fmt.Printf("%T\n", marshalValues)
+		if marshalErr != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(201)
+		w.Write(returnData)
+
 	})
 
 	err := server.ListenAndServe()
